0d3ccd982378feb0c9a5e9b8abf042ef9af263b1
Wanderson notebook <wandersonlife7@gmail.com>
2025-02-03 16:15:55 -0300

    estilos iniciais

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,18 @@
-import React from "react";
-import "./App.css";
+// CSS
+import styles from "./App.module.css";
 
-// hooks
-import { useState } from "react";
+// components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-  const [text, setText] = useState<string | null>(null);
-
   return (
-    <div className="App">
-      <h1>React + typescript</h1>
-      <hr />
-      <form onSubmit={(e) => e.preventDefault}>
-        <input type="text" placeholder="Digite sua tarefa" />
-        <input
-          type="button"
-          onClick={() => {
-            setText("fazendo tarefa");
-          }}
-        />
-      </form>
-      {text ? (
-        <div>
-          <p>{text}</p>
-        </div>
-      ) : (
-        <div>
-          <p>Sem tarefas</p>
-        </div>
-      )}
+    <div>
+      <Header />
+      <main className={styles.main}>
+        <h2>Conteudo....</h2>
+      </main>
+      <Footer />
     </div>
   );
 }
