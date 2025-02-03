139f8a2619651a4d83bfc958d807a6916f8bdc5a
Wanderson notebook <wandersonlife7@gmail.com>
2025-02-02 22:53:08 -0300

    just exercising

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+// hooks
+import { useState } from "react";
 
 function App() {
+  const [text, setText] = useState<string | null>(null);
+
   return (
     <div className="App">
       <h1>React + typescript</h1>
+      <hr />
+      <form onSubmit={(e) => e.preventDefault}>
+        <input type="text" placeholder="Digite sua tarefa" />
+        <input
+          type="button"
+          onClick={() => {
+            setText("fazendo tarefa");
+          }}
+        />
+      </form>
+      {text ? (
+        <div>
+          <p>{text}</p>
+        </div>
+      ) : (
+        <div>
+          <p>Sem tarefas</p>
+        </div>
+      )}
     </div>
   );
 }
