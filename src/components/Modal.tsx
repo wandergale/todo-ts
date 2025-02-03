408bb39a8ae5e874486f3a084a153ec1ba88af78
Wanderson notebook <wandersonlife7@gmail.com>
2025-02-03 18:20:01 -0300

    fechar modal ao clicar fora

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -5,9 +5,15 @@ interface IProps {
 }
 
 const Modal = ({ children }: IProps) => {
+  const closeModal = (e: React.MouseEvent): void => {
+    const modal = document.querySelector("#modal");
+
+    modal!.classList.add("hide");
+  };
+
   return (
     <div id="modal">
-      <div className={styles.fade}></div>
+      <div className={styles.fade} onClick={closeModal}></div>
       <div className={styles.modal}>
         <h2>Texto modal</h2>
         {children}
